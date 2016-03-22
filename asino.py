d17a4f086359a0977c5e0c93c147c1375e0abc60
Francesco Mecca <s211897@studenti.polito.it>
2016-03-22 10:30:32 +0100

    modified asino.py

--- asino.py
+++ asino.py
@@ -1,10 +1,7 @@
-import random
-import sys
-import datetime
+import random, sys
 
 if __name__ == '__main__':
     args = list()
-    start = datetime.datetime.now()
     if not sys.stdin.isatty():
         for line in sys.stdin:
             args.append(line)
@@ -13,5 +10,4 @@ if __name__ == '__main__':
     argRange = len(args)
     for i in range(argRange):
         print(str(i+1) + '.', args.pop(random.randrange(0, len(args))))    
-    end = datetime.datetime.now()
     print('\n', start, end)
